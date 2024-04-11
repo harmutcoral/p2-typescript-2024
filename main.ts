aafcdcad7e68d675d418d8f1c9358170b24be0a6
harmutcoral <coral.harmut98@gmail.com>
2024-04-11 14:47:45 +0200

    add API call and index html

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 import { writeFile } from "fs/promises";
 import { render } from "./render.js";
-import { loadUsers } from "./users.js";
+import { loadPokemons } from "./pokemon.js";
 
-const users = await loadUsers(100);
-const html = render(users);
-await writeFile('users.html', html);
 
+const pokemons = await loadPokemons(100);
+const html = render(pokemons);
+await writeFile('pokemons.html', html);
---
+++ pokemon.ts
@@ -0,0 +1,25 @@
+export class Pokemon {
+  constructor(
+    public id: number,
+    public name: string,
+    public types: string[],
+    public abilities: string[],
+    public imageUrl: string
+  ) {}
+}
+
+export const loadPokemons = async (n: number) => {
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${n}`);
+  const { results } = (await response.json()) as { results: any[] };
+  const pokemons: Array<Pokemon> = [];
+  for (const { name, url } of results) {
+    const pokemonResponse = await fetch(url);
+    const pokemonData = await pokemonResponse.json();
+    const id = pokemonData.id;
+    const types = pokemonData.types.map((type: any) => type.type.name);
+    const abilities = pokemonData.abilities.map((ability: any) => ability.ability.name);
+    const imageUrl = pokemonData.sprites.front_default;
+    pokemons.push(new Pokemon(id, name, types, abilities, imageUrl));
+  }
+  return pokemons;
+};
