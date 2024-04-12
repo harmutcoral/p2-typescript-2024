8440eb71fd7d0053a71f37573e3540ec7f811a92
harmutcoral <coral.harmut98@gmail.com>
2024-04-12 15:45:13 +0200

    update css to main html

--- render.ts
+++ render.ts
@@ -6,42 +6,57 @@ const head = (title: string) => `
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>${title}</title>
+
   <style>
-    body {
-      margin: 0;
-      padding: 0;
-    }
-    .pokemon {
-      font-family: sans-serif;
-      display: flex;
-      flex-direction: row;
-      align-items: center;
-      padding: .4rem;
-      border-bottom: 1px solid #ddd;
-      cursor: pointer; 
-    }
-    .pokemon img {
-      width: 3rem;
-      height: 3rem;
-      margin-right: 0.7rem;
-    }
-    .pokemon .name {
-      font-weight: bold;
-    }
-    .pokemon .type {
-      font-family: monospace;
-    }
-  </style>
+  body {
+    margin: 0;
+    padding: 0;
+  }
+  
+  .pokemon {
+    font-family: sans-serif;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    padding: .4em;
+    border-bottom: 1px solid #ddd;
+    cursor: pointer; 
+    text-decoration: none;
+    color: #333;
+  }
+  
+  .pokemon img {
+    width: 5em;
+    height: 5em;
+    margin-right: 1em;
+  }
+  
+  .pokemon .data {
+    display: flex;
+    flex-direction: column;
+  }
+  
+  .pokemon .name {
+    font-weight: bold;
+    margin-bottom: 0.2em; 
+  }
+  
+  .pokemon .type {
+    font-family: monospace;
+    margin: 0; 
+  }
+</style>
 </head>`;
 
 const renderPokemons = (pokemons: Pokemon[]) => {
   let html = "";
   for (const pokemon of pokemons) {
-    html += `<a href="pokemon-data/${pokemon.name.toLowerCase()}.html" class="pokemon">
-      <img src="${pokemon.imageUrl}" />
+    html += 
+    `<a href="pokemon-data/${pokemon.name.toLowerCase()}.html" class="pokemon">
+      <img src="${pokemon.imageUrl}" alt="${pokemon.name}" />
       <div class="data">
-        <div class="name">${pokemon.name}</div>
-        <div class="type">${pokemon.types.join(", ")}</div>
+        <h3 class="name">${pokemon.name}</h3>
+        <p class="type">${pokemon.types.join(", ")}</p>
       </div>
     </a>`;
   }
